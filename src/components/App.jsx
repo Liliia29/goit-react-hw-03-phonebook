443e9b5743cd694a5f53cdeace12225e6b59b632
Liliia29 <118195732+Liliia29@users.noreply.github.com>
2023-06-23 19:55:49 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,34 +11,33 @@ import {
   NoContacts,
 } from './App.styled';
 
-
 const CONTACTS_LS_KEY = 'contacts';
 const initialContacts = [
-      { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
-      { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
-      { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
-      { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
-    ];
+  { id: 'id-1', name: 'Rosie Simpson', number: '459-12-56' },
+  { id: 'id-2', name: 'Hermione Kline', number: '443-89-12' },
+  { id: 'id-3', name: 'Eden Clements', number: '645-17-79' },
+  { id: 'id-4', name: 'Annie Copeland', number: '227-91-26' },
+];
 
-    export class App extends Component {
+export class App extends Component {
   state = {
     contacts: [],
     filter: '',
   };
 
-  componentDidMount(){
-    const saveContacts = localStorage.getItem(CONTACTS_LS_KEY);
+  componentDidMount() {
+    const savedContacts = localStorage.getItem(CONTACTS_LS_KEY);
 
-    if (saveContacts !== null) {
-      const parsedContacts = JSON.parse(saveContacts);
-      this.setState({contacts: parsedContacts});
+    if (savedContacts !== null) {
+      const parsedContacts = JSON.parse(savedContacts);
+      this.setState({ contacts: parsedContacts });
       return;
     }
-    this.setState({contacts: initialContacts});
+    this.setState({ contacts: initialContacts });
   }
 
-  componentDidUpdate(_, prevState){
-    if (this.state.contacts !== prevState.contacts){
+  componentDidUpdate(_, prevState) {
+    if (this.state.contacts !== prevState.contacts) {
       localStorage.setItem(
         CONTACTS_LS_KEY,
         JSON.stringify(this.state.contacts)
@@ -74,30 +73,30 @@ const initialContacts = [
   removeContact = contactId => {
     this.setState(prevState => {
       return {
-        contacts: prevState.contacts.filter(({id}) => id !== contactId),
+        contacts: prevState.contacts.filter(({ id }) => id !== contactId),
       };
     });
   };
 
-    render() {
-      const { filter, contacts } = this.state;
-      const visibleContacts = this.getVisibleContacts();
-       return (
-        <Container>
-          <Title>Phonebook</Title>
-          <ContactForm onSubmit={this.handleFormSubmit}/>
-          <ContactsTitle>Contacts</ContactsTitle>
-          <FilterTitle>Find contacts by name</FilterTitle>
-          <Filter value={filter} onFilterChange={this.handleFilterChange}/>
-          {contacts.length ? (
-            <ContactList
-              contacts={visibleContacts}
-              onRemove={this.removeContact}
-            />
-          ) : (
-            <NoContacts>No contacts added yes.</NoContacts>
-          )}
-        </Container>
-      );
-    }
+  render() {
+    const { filter, contacts } = this.state;
+    const visibleContacts = this.getVisibleContacts();
+    return (
+      <Container>
+        <Title>Phonebook</Title>
+        <ContactForm onSubmit={this.handleFormSubmit} />
+        <ContactsTitle>Contacts</ContactsTitle>
+        <FilterTitle>Find contacts by name</FilterTitle>
+        <Filter value={filter} onFilterChange={this.handleFilterChange} />
+        {contacts.length ? (
+          <ContactList
+            contacts={visibleContacts}
+            onRemove={this.removeContact}
+          />
+        ) : (
+          <NoContacts>No contacts added yes.</NoContacts>
+        )}
+      </Container>
+    );
+  }
 }
